4badf8ea6f390aeb26ec60833f3fcb705c109b8f
wukun <wukun@hlchang.cn>
2019-04-25 10:52:05 +0800

    完成数据库mybatis相关内容

--- src/main/java/com/miaoshaproject/MiaoshaApplication.java
+++ src/main/java/com/miaoshaproject/MiaoshaApplication.java
@@ -1,10 +1,31 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
+@MapperScan("com.miaoshaproject.dao")
+@RestController
 public class MiaoshaApplication {
+
+	@Autowired
+	private UserDOMapper userDOMapper;
+
+	@GetMapping("/")
+	public String hello() {
+		UserDO userDO = userDOMapper.selectByPrimaryKey(6);
+		if (userDO == null)
+			return "用户对象不存在";
+		else
+			return userDO.getName();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiaoshaApplication.class, args);
