05cf0914758d3a05fb30c6eb80fa9b063bab20f4
wukun <wukun@hlchang.cn>
2019-04-26 11:46:09 +0800

    通用返回-错误返回：EmBusinessError、BusinessException、exceptionHandler

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,28 +1,37 @@
 package com.miaoshaproject.controller;
 
 import com.miaoshaproject.controller.viewobject.UserVO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
 import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
 
     @RequestMapping("/get")
     @ResponseBody
-    public CommonReturnType getUser(@RequestParam(name = "id") Integer id) {
+    public CommonReturnType getUser(@RequestParam(name = "id") Integer id) throws BusinessException {
         // 调用service服务获取对应id的用户对象，并返回给前端
         UserModel userModel = userService.getUserById(id);
+        if (userModel == null) {
+//            userModel.setEncrptPassword("a"); // 测试返回'未知错误'
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
         UserVO userVO = convertFromModel(userModel);
         return CommonReturnType.create(userVO);
     }
---
+++ src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -0,0 +1,35 @@
+package com.miaoshaproject.error;
+
+public enum EmBusinessError implements CommonError {
+
+    // 10000开头的为通用的错误定义.其中errMsg可被重置 by setErrMsg，例如重置为'邮箱格式错误'
+    PARAMETER_VALIDATION_ERROR(10001, "参数不合法"),
+    // 未知错误
+    UNKNOWN_ERROR(10002, "未知错误"),
+
+    // 20000开头的为用户模块的错误定义
+    USER_NOT_EXIST(20001, "用户不存在");
+
+    private int errCode;
+    private String errMsg;
+
+    EmBusinessError(int errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+    @Override
+    public int getErrCode() {
+        return this.errCode;
+    }
+
+    @Override
+    public String getErrMsg() {
+        return this.errMsg;
+    }
+
+    @Override
+    public CommonError setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+        return this;
+    }
+}
