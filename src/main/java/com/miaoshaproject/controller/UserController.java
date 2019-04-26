9eacdff2a84b4e4beac7ea0fd116df4873b72520
wukun <wukun@hlchang.cn>
2019-04-26 17:08:01 +0800

    otp验证码获取

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -8,13 +8,11 @@ import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
@@ -22,6 +20,32 @@ public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    /** 以Bean的方式注入了进来，就代表 httpServletRequest是单例模式。
+     * 那怎么支持多个用户的并发访问呢？
+     * 通过Spring Bean包装的httpServletRequest，实际上是一个 proxy，内部有 ThreadLocal方式的map，不同的用户有不同的线程来区分。
+     */
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    // 用户获取otpCode短信
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+        // 生成 otpCode
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);  // [0, 99999)
+        randomInt += 10000;  // [10000, 109999)
+        String otpCode = String.valueOf(randomInt);
+
+        // 将 otpCode 同对应的用户手机号关联。使用 httpSession方式，分布式适合使用 Redis
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+        // 发送 otpCode 给用户
+        System.out.println("telphone = " + telphone + " & otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
