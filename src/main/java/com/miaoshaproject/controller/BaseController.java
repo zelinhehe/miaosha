f93474b7939e22a64ebbd7eed370bc6623329896
wukun <wukun@hlchang.cn>
2019-05-16 17:49:08 +0800

    定义全局异常处理，避免404、405返回错误页面而不是自定义的json

--- src/main/java/com/miaoshaproject/controller/BaseController.java
+++ src/main/java/com/miaoshaproject/controller/BaseController.java
@@ -17,21 +17,21 @@ public class BaseController {
     public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
 
     // 定义exceptionHandler来捕获未被Controller层捕获的exception
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception ex) {
-        Map<String, Object> responseData = new HashMap<>();
-
-        if (ex instanceof BusinessException) {
-            BusinessException businessException = (BusinessException)ex;
-            responseData.put("errCode", businessException.getErrCode());
-            responseData.put("errMsg", businessException.getErrMsg());
-        } else {
-            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
-        }
-
-        return CommonReturnType.create(responseData, "fail");
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Object handlerException(HttpServletRequest request, Exception ex) {
+//        Map<String, Object> responseData = new HashMap<>();
+//
+//        if (ex instanceof BusinessException) {
+//            BusinessException businessException = (BusinessException)ex;
+//            responseData.put("errCode", businessException.getErrCode());
+//            responseData.put("errMsg", businessException.getErrMsg());
+//        } else {
+//            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+//            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+//        }
+//
+//        return CommonReturnType.create(responseData, "fail");
+//    }
 }
---
+++ src/main/java/com/miaoshaproject/controller/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.miaoshaproject.controller;
+
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.response.CommonReturnType;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public CommonReturnType doError(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Exception ex) {
+        ex.printStackTrace();
+        Map<String, Object> responseData = new HashMap<>();
+        if (ex instanceof BusinessException) {
+            BusinessException businessException = (BusinessException)ex;
+            responseData.put("errCode", businessException.getErrCode());
+            responseData.put("errMsg", businessException.getErrMsg());
+        } else if (ex instanceof ServletRequestBindingException) {  // 解决405的返回信息。例如必传参数url中没有
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", "URL 绑定路由问题");
+        } else if (ex instanceof NoHandlerFoundException) {  // 解决404的返回信息 + 两个配置项
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", "没有找到对应的访问路径");
+        } else {
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+        }
+        return CommonReturnType.create(responseData, "fail");
+    }
+}
